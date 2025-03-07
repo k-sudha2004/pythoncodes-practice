e6a856a4ddaf9be29c12107ab95d18e5e9a7885d
k-sudha2004 <143861949+k-sudha2004@users.noreply.github.com>
2025-03-07 19:22:05 +0530

    Add files via upload

---
+++ p26.py
@@ -0,0 +1,9 @@
+#EXAMS
+T = int(input())
+for _ in range(T):
+    X, Y, Z = map(int, input().split())
+    total_students = X * Y
+    if 2 * Z > total_students:
+        print("YES")
+    else:
+        print("NO")
