76f5087995c02e1aeeed5ecabb56a0b895dfd3ed
k-sudha2004 <143861949+k-sudha2004@users.noreply.github.com>
2025-04-09 18:46:03 +0530

    Add files via upload

---
+++ p59.py
@@ -0,0 +1,7 @@
+T = int(input())
+for _ in range(T):
+    N, A, B = map(int, input().split())
+    odd = (N + 1) // 2
+    even = N // 2
+    total_duration = (odd * B) + (even * A)
+    print(total_duration)
